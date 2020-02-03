9a316f66d07d3e79b128d4c3ee95eaeef83a3bbb
Manvitha Konkimalla <manvitha041@gmail.com>
2020-02-03 20:11:46 +0100

    Updates

---
+++ precache-manifest.8f6507bfff093a3cc11b0138538240a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e88f72a0c0414883e5fc1bb001e467",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b4df8e94b4d62cd402d",
+    "url": "./static/css/main.a036f5aa.chunk.css"
+  },
+  {
+    "revision": "a5cbd1ded5ca90dcff3f",
+    "url": "./static/js/2.bd09e5e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.bd09e5e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b4df8e94b4d62cd402d",
+    "url": "./static/js/main.fae383fe.chunk.js"
+  },
+  {
+    "revision": "6f1cb0f1d4d9c8208063",
+    "url": "./static/js/runtime-main.d6441537.js"
+  }
+]);
